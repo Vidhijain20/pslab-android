cd8c8d901928b5f768fe347f9ff450b46ce8b230
Anashuman Singh <anash.cheema@gmail.com>
2024-06-06 09:09:26 +0530

    fix: fixes waveform displaying issues

--- app/src/main/java/io/pslab/activity/OscilloscopeActivity.java
+++ app/src/main/java/io/pslab/activity/OscilloscopeActivity.java
@@ -1193,6 +1193,8 @@ public class OscilloscopeActivity extends GuideActivity implements View.OnClickL
             xAxisScale = maxTimebase;
         }
         yAxisScale = maxY + yPadding;
+        samples = 512;
+        timeGap = (xAxisScale * 1000.0) / samples;
     }
 
     public class XYPlotTask extends AsyncTask<String, Void, Void> {
